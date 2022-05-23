6d894d4b47daa945706769eb9330f9132e702a96
Iwan-Bos <iwanbos05alt@gmail.com>
2022-05-23 13:21:34 +0200

    removed da big wall

---
+++ Assets/Scripts/UIPanelToggles.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPanelToggles : MonoBehaviour {
+
+    // Panels;
+    public GameObject MainPanel;
+    public GameObject OptionsPanel;
+    public GameObject CreditsPanel;
+
+    // Active on load configuration;
+    private void Start() {
+        MainPanel.SetActive(true);
+        OptionsPanel.SetActive(false);
+        CreditsPanel.SetActive(false);
+    }
+    // UI Toggles;
+    public void OpenOptions() {
+        if (OptionsPanel != null) {
+            MainPanel.SetActive(false);
+            OptionsPanel.SetActive(true);
+        }
+    }
+    public void CloseOptions() {
+        if (MainPanel != null) {
+            OptionsPanel.SetActive(false);
+            MainPanel.SetActive(true);
+        }
+    }
+    public void OpenCredits() {
+        if (CreditsPanel != null) {
+            MainPanel.SetActive(false);
+            CreditsPanel.SetActive(true);
+        }
+    }
+    public void CloseCredits() {
+        if (CreditsPanel != null) {
+            CreditsPanel.SetActive(false);
+            MainPanel.SetActive(true);
+        }
+    }
+    
+    // Misc UI Functions;
+    public void QuitGame() { 
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+    public void PauseGame() { Time.timeScale = 0.0f; }
+    public void ResumeGame() { Time.timeScale = 1.0f; }
+
+}
