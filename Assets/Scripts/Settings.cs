168752c32918032b92cb16d91737e67f63b77b6d
Iwan-Bos <iwanbos05alt@gmail.com>
2022-06-10 15:13:09 +0200

    resolutions & screen modes

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -10,42 +10,55 @@ public class Settings : MonoBehaviour
     public AudioMixer audioMixer;
     public Dropdown resolutionDropdown;
     public Dropdown screenModeDropdown;
-    Resolution[] resolutions;
-    List<string> resolutionOptions = new List<string>();
-    Dictionary<int, FullScreenMode> screenMode = new Dictionary<int, FullScreenMode>();
-    int resolutionIndex = 0;
+    private Resolution[] resolutions;
+    private List<string> resolutionOptions = new List<string>();
+    private Dictionary<int, FullScreenMode> screenMode = new Dictionary<int, FullScreenMode>();
 
-    private void Start() 
-    {
-        // Gets all possible resolutions for this device and puts it in Resolution[] (an array of the Type Resolution) ;
-        // Converts every Resolution to a string and puts it in a List<string> ;
-        // Adds the List<string> of options to the resolutionDropdown ;
-        // If This is the current screen resolution sets it as the dropdown index ;
-        
+    private void Start()  {
+
+        // start the game in fullscreen
+        Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+
+        // get all possible resolutions for this device
         resolutions = Screen.resolutions;
+        // clear the current resolutionDropdown options
         resolutionDropdown.ClearOptions();
+        
+        // convert every Resolution to a string and put it in resolutionOptions
         for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].ToString();
             resolutionOptions.Add(option);
 
-            if (resolutions[i].ToString() == Screen.currentResolution.ToString()) { resolutionIndex = i; }
+            // if this is the current screen resolution ; set it as the dropdown index
+            if (resolutions[i].ToString() == resolutions[i].ToString()) 
+            { 
+                resolutionDropdown.value = i; 
+            }
         }
-        resolutionDropdown.AddOptions(resolutionOptions);
-        resolutionDropdown.value = resolutionIndex;
 
+        // add the List of options to resolutionDropdown
+        resolutionDropdown.AddOptions(resolutionOptions);
+
+        // add the screenMode List items
         screenMode.Add(0, FullScreenMode.ExclusiveFullScreen);
         screenMode.Add(1, FullScreenMode.FullScreenWindow);
         screenMode.Add(2, FullScreenMode.MaximizedWindow);
         screenMode.Add(3, FullScreenMode.Windowed);
     }
+
+    public void SetResolution() {
+
+        Resolution sResolution = resolutions[resolutionDropdown.value];
+        FullScreenMode sScreenMode = screenMode[screenModeDropdown.value];
+
+       // set the resolution with the selected values
+        Screen.SetResolution(sResolution.width, sResolution.height, sScreenMode, sResolution.refreshRate);
+    }
+    
+    // methods for changing audio mixer values
     public void SetMasterVolume(float volume) { audioMixer.SetFloat("MasterVolume", volume); }
     public void SetMusicVolume(float volume) { audioMixer.SetFloat("MusicVolume", volume); }
     public void SetSFXVolume(float volume) { audioMixer.SetFloat("SFXVolume", volume); }
 
-    public void SetResolution() {
-        Resolution selectedResolution = resolutions[resolutionIndex];
-        FullScreenMode selectedScreenMode = screenMode[screenModeDropdown.value];
-        Screen.SetResolution(selectedResolution.height, selectedResolution.width, selectedScreenMode, selectedResolution.refreshRate);
-    }
 }
